1577c581f86085af77a74a0208c1126b1c414a91
Mark <github@markedmondson.me>
2025-11-16 15:40:02 +0100

    cleanup

--- data.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, name: str, age: int, email: str) -> None:
-        self.name = name
-        self.age = age
-        self.email = email
-
-
-def validateEmail(email: str) -> bool:
-    return "@" in email
